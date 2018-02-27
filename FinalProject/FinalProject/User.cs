789bdd93f889e3b3fe6334367a0e8dfb7c1e620c
TYLERS-PC146\tyler <marl7950@bears.unco.edu>
2018-02-27 09:54:38 -0700

    User class added, still cannot get tweet invi recognized

---
+++ FinalProject/FinalProject/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class User
+    {
+        public string name { get; set; }
+        public string handle { get; set; }
+        public System.Drawing.Bitmap image { get; set; }
+
+        public void Retweet(Tweet currentTweet)
+        {
+            int retweet = currentTweet.retweets;
+            retweet += 1;
+            currentTweet.retweets = retweet;
+            /*Copy tweet body and then post it to timeline**/
+        }
+        public void Like(Tweet currentTweet)
+        {
+            int like = currentTweet.likes;
+            like += 1;
+            currentTweet.likes = like;
+        }
+        public void Logout()
+        {
+
+        }
+
+    }
+}
