8b8139f6ba67fa39e7a9a8d3a656d3aac728da80
STUDENT1\elmo4982 <elmo4982@bears.unco.edu>
2018-04-10 09:16:05 -0600

    Accoutn class created and sign up functionality started check credentials method created.

---
+++ FinalProject/FinalProject/Model Classes/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FinalProject
+{
+    class Account
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        
+
+        public Account(string name, string password)
+        {
+            Username = name;
+            Password = password;
+            StreamWriter sw = new StreamWriter("Credentials.txt");
+            sw.WriteLine(Username," ",Password);
+            sw.Close();
+        }
+    }
+}
